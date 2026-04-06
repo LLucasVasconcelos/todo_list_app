ad234e96b8e16f2326dac20d2f8bc52aa94fd403
LLucasVasconcelos <llucas.vasconcelos1@gmail.com>
2026-04-06 18:22:53 -0300

    Creating First files, and add app documentation

---
+++ lib/data/task_data.dart
@@ -0,0 +1,12 @@
+import 'package:todo_list_app/models/task.dart';
+
+List<Task> tasks = [
+  Task(
+    name: "Learn Flutter",
+    description: "Review basics widgets and project structure"
+  ),
+  Task(
+    name: "Do Homework",
+    description: "Create to-do list Screen",
+    ),
+];
---
+++ lib/models/task.dart
@@ -0,0 +1,10 @@
+class Task {
+  final String name;
+  final String description;
+
+  Task({
+    required this.name,
+    required this.description
+  });
+
+}
